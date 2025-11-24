0ac352d6d4d429acf724891c8fa93e5e8136eeaf
saintjin0 <byoungikhwang@gmail.com>
2025-11-24 17:32:28 +0900

    list.html

--- main.py
+++ main.py
@@ -24,9 +24,43 @@ async def root_html():
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 templates = Jinja2Templates(directory="templates/")
+
 # http://localhost:8000/main_html
 @app.get("/main_html")
 async def main_html(request: Request):
     return templates.TemplateResponse("main.html"
                                       , {"request": request})
+
+# http://localhost:8000/main_context_html
+@app.get("/main_html_context")
+async def main_html_context(request: Request):
+    # 템플렛에 전달할 데이타
+    context ={
+        "request": request,
+        "title": "fastapi + jinja exaple",
+        "itmes": ["apple", "banama","cherry"],
+        "user": {"name: sanghun"
+                 , "age: 33"}
+    }
+    
+
+    return templates.TemplateResponse("main_context.html"
+                                      , context)
+
+# http://localhost:8000/
+@app.get("/users_list")
+async def user_list(request: Request):
+    context = {
+        "request": request,
+        "user_list : users"
+        }
+    return templates.TemplateResponse("users/list.html"
+                                      , context)
+
+# 정적 파일 설정
+from fastapi.staticfiles import StaticFiles
+app.mount("/images", StaticFiles(directory = "resources/images"))
+app.mount("/css", StaticFiles(directory = "resources/css"))    
+
+
 pass
