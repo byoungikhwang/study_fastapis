fbb1dc2e48f91d2243f2a02e36a55532f94ce8e0
saintjin0 <byoungikhwang@gmail.com>
2025-11-24 14:21:13 +0900

    main.py

--- main.py
+++ main.py
@@ -1,52 +1,32 @@
-# # FastAPI 기본 구현
 from fastapi import FastAPI
-app = FastAPI()             # 웹 네트웍에서 함수를 사용하기 위해 app에 담음
-#http://localhost:8000/html 
-@app.get("/html")           # 펑션을 호출
+app = FastAPI()
+# http://localhost:8000/
+@app.get("/")
 async def root():
-    return {"message": "saintjin Hello, World!"}
-def root_html():
+    return {"message": "Hello, World!"}
+# http://localhost:8000/html
+@app.get("/html")
+async def root_html():
     html_content = '''
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
+            <title>Otter</title>
         </head>
         <body>
-            <div>My name saintjin </div>
+            <div>My name is Otter!</div>
         </body>
         </html>
         '''
     return html_content
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+templates = Jinja2Templates(directory="templates/")
+# http://localhost:8000/main_html
+@app.get("/main_html")
+async def main_html(request: Request):
+    return templates.TemplateResponse("main.html"
+                                      , {"request": request})
 pass
-
-
-
-
-
-
-
-
-# # FastAPI 기본 구현
-# from fastapi import FastAPI
-
-# app = FastAPI()         # 웹 네트웍에서 함수를 사용하기 위해 app에 담음
-
-# @app.get("/")           # 펑션을 호출
-# async def root():
-    
-# <!DOCTYPE html>
-# <html lang="en">
-# <head>
-#     <meta charset="UTF-8">
-#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#     <title>Document</title>
-# </head>
-# <body>
-# <div> my name saintjin </div>
-# </body>
-# </html>
-#     return {"message": "Hello World"}
-# pass
